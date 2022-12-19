69fccee915f4b60659751627447329258cf65388
Mayowa Ajiboye <mayaajiboye@gmail.com>
2022-12-19 18:22:08 -0500

    commit tree 3b7347f21260ac10cd896cb4b941e7fba6e26d31

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+  cats.push('Ralph')
+
+}
+function destructivelyPrependCat(name){
+  cats.unshift('Bob')
+
+}
+function destructivelyRemoveLastCat(name){
+  cats.pop('Garfield')
+
+}
+function destructivelyRemoveFirstCat(name){
+  cats.shift('Milo')
+
+}
+
+function appendCat(name){
+  const copyCats = [...cats, 'Broom'];
+  return copyCats;
+
+}
+
+
+function prependCat(name){
+  const copyCats = [ 'Arnold', ...cats,];
+  return copyCats;
+
+}
+
+function  removeLastCat(){
+  const copyCats = cats.slice(0, cats.length-1);
+  return copyCats;
+
+}
+function  removeFirstCat(){
+  const copyCats = cats.slice(1);
+  return copyCats;
+
+}
+
+
+
+
+
+
